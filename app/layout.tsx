c04655e62d755cda786af7bfcf4ae0c80b6ebd43
05rahmatulloh <05rahmatulloh@gmail.com>
2024-11-27 18:06:31 +0700

    Update layout.tsx
    menambahkan  script google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10857947855"></script>
+        <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-10857947855'); </script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
